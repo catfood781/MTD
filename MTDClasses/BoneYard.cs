52fd549cf1c90f8adc0e1097453aa59e1adf7695
catfood781 <46457994+catfood781@users.noreply.github.com>
2019-04-15 17:22:12 -0700

    adding files from drive

---
+++ MTDClasses/BoneYard.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTDClasses
+{
+    public class BoneYard
+    {
+        private List<Domino> dList;
+        public int dominos => dList.Count;
+
+        public BoneYard(int maxDots)
+        {
+            dList = new List<Domino>();
+            for (int side1 = 0; side1 <= maxDots; side1++)
+            {
+                for (int side2 = side1; side2 <= maxDots; side2++)
+                {
+                    dList.Add(new Domino(side1, side2));
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < dList.Count; i++)
+            {
+                int randIndex = rand.Next(0, dList.Count);
+                Domino firstDomino = dList[i];
+                Domino secondDomino = dList[randIndex];
+
+                dList[i] = secondDomino;
+                dList[randIndex] = firstDomino;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (dominos == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int DominosRemaining
+        {
+            get { return dList.Count; }
+        }
+
+        public Domino Draw()
+        {
+            Domino firstDomino = dList[0];
+            dList.Remove(firstDomino);
+            return firstDomino;
+        }
+
+        public Domino this[int i]
+        {
+            get { return dList[i]; }
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+
+            foreach (Domino d in dList)
+            {
+                output += d.ToString() + "\n";
+
+            }
+            return "";
+        }
+
+    }
+}
---
+++ MTDTests/BoneYardTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using MTDClasses;
+
+namespace MTDTests
+{
+    [TestFixture]
+    public class BoneYardTests
+    {
+        BoneYard boneYard;
+        BoneYard boneYardOverloaded;
+        BoneYard boneNeg1;
+        BoneYard bone1;
+        int maxDots = 12;
+
+        [SetUp]
+        public void SetUpAllTests()
+        {
+            boneYard = new BoneYard(0);
+            boneYardOverloaded = new BoneYard(maxDots);
+            boneNeg1 = new BoneYard(-1);
+            bone1 = new BoneYard(1);
+        }
+        [Test]
+        public void TestSimpleAddition()
+        {
+            int math = 1 + 2;
+            Assert.AreEqual(3, math);
+        }
+        [Test]
+        public void TestOverloadedConstructor()
+        {
+            Assert.AreEqual(1, boneYard.DominosRemaining);
+            Assert.AreEqual(91, boneYardOverloaded.DominosRemaining);
+            Assert.AreEqual(0, boneNeg1.DominosRemaining);
+            Assert.AreEqual(3, bone1.DominosRemaining);
+        }
+        [Test]
+        public void TestBoneYardShuffle()
+        {
+            boneYardOverloaded.Shuffle();
+            Domino firstDomino = boneYardOverloaded[0];
+            Assert.AreNotEqual(0, firstDomino.Side1);
+            Assert.AreNotEqual(0, firstDomino.Side2);
+        }
+        [Test]
+        public void TestBoneyardIsEmpty()
+        {
+            Assert.AreEqual(false, boneYardOverloaded.IsEmpty());
+            Assert.AreEqual(true, boneNeg1.IsEmpty());
+        }
+        [Test]
+        public void TestDominosRemaining()
+        {
+            Assert.AreEqual(1, boneYard.DominosRemaining);
+            Assert.AreEqual(91, boneYardOverloaded.DominosRemaining);
+            Assert.AreEqual(0, boneNeg1.DominosRemaining);
+            Assert.AreEqual(3, bone1.DominosRemaining);
+        }
+        [Test]
+        public void TestBoneyardDraw()
+        {
+            Domino firstDomino = boneYardOverloaded.Draw();
+            Domino secondDomino = boneYardOverloaded.Draw();
+            Assert.AreEqual(0, firstDomino.Side1);
+            Assert.AreEqual(0, firstDomino.Side2);
+            Assert.AreEqual(0, secondDomino.Side1);
+            Assert.AreEqual(1, secondDomino.Side2);
+        }
+    }
+}
